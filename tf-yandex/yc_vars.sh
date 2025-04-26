9063826a1b5b9c9ba5303fc8cf0cc39a9a804443
MikhailCherepanovD <mikhail.cherepanov.2002@mail.ru>
2025-04-26 18:06:57 +0300

    2604

---
+++ tf-yandex/yc_vars.sh
@@ -0,0 +1,11 @@
+#/usr/bin/env bash
+
+YC_TOKEN=$($HOME/yandex-cloud/bin/yc iam create-token)
+YC_CLOUD_ID=$($HOME/yandex-cloud/bin/yc config get cloud-id)
+YC_FOLDER_ID=$($HOME/yandex-cloud/bin/yc config get folder-id)
+
+echo $(jq --null-input \
+          --arg token "$YC_TOKEN" \
+          --arg cloud_id "$YC_CLOUD_ID" \
+          --arg folder_id "$YC_FOLDER_ID" \
+          '{"token": $token, "cloud_id": $cloud_id, "folder_id": $folder_id}')
